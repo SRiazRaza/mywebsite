4cebe4edab5895f3976dbf37ca3ca31b8135e71b
SRiazRaza <riazraza0@gmail.com>
2019-11-19 19:54:47 +0500

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
-import React from "react"
+import { graphql } from 'gatsby'
+import React from 'react'
+import Header from '../components/Home/HeaderHome'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+import { pages } from '../utils'
 
-export default () => <div>Hello world!</div>
+class Index extends React.Component {
+  render() {
+    const { siteUrl } = this.props.data.site.siteMetadata
+    return (
+      <Layout location={this.props.location} active={pages.home}>
+        <SEO title="" url={siteUrl} />
+        <Header />
+        <div
+          style={{
+            background: '#22292c',
+            height: '200px'
+          }}
+        />
+      </Layout>
+    )
+  }
+}
+
+export const queryBlog = graphql`
+  query {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`
+
+export default Index
