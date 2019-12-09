23e04f1b81497d1ec54e301e228a9a2e465f938a
SRiazRaza <riazraza0@gmail.com>
2019-12-09 23:53:10 +0500

    a

--- src/components/about.js
+++ src/components/about.js
@@ -1,86 +1,103 @@
 import React from 'react'
 import Img from 'gatsby-image'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
+import { graphql, StaticQuery } from 'gatsby'
 
-import SEO from './SEO'
-import Layout from '../components/Layout'
-
-import Header from '../components/Header'
+import { media } from './styles'
 
 import { pages } from '../utils'
 
-class About extends React.Component {
-  render() {
-    const siteUrl = get(this, 'props.data.site.siteMetadata.siteUrl')
-    return (
-      <Layout location={this.props.location} active={pages.about}>
-        <div className="About">
-          <SEO title="About" url={`${siteUrl}/about`} />
-          <Header
-            title="ABOUT"
-            description="Hi, I'm Dante Calderón, web developer. I like to build things with Nodejs and I work with React, Redux, Gatsby, Nextjs and the entire React ecosystem."
-            color="#3384a0"
-          />
-          <div className="container">
-            <div className="row center-xs">
-              <div className="About__img-container col-xs-12 col-md-6">
-                <div className="About__img">
-                  <Img sizes={this.props.data.aboutImage.sizes} />
-                </div>
-              </div>
-              <div className="About__interests col-xs-12 col-md-6 text-left first-md">
-                <h3 className="About__interests__title">Interests</h3>
-                <ul className="About__interests__list">
-                  <li className="About__interests__item">
-                    <i className="ai" />
-                    Artificial Intelligence
-                  </li>
-                  <li className="About__interests__item">
-                    <i className="ml" />
-                    Machine Learning
-                  </li>
-                  <li className="About__interests__item">
-                    <i className="ds" />
-                    Data Science
-                  </li>
-                  <li className="About__interests__item">
-                    <i className="cp" />
-                    Competitive Programming
-                  </li>
-                  <li className="About__interests__item">
-                    <i className="game" />
-                    Games Development
-                  </li>
-                  <li className="About__interests__item">
-                    <i className="back" />
-                    Backend Development
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-       
-        </div>
-      </Layout>
-    )
-  }
-}
 
-export const queryAbout = graphql`
-  query QueryAbout {
+
+const Container = styled.div`
+  min-height: 600px;
+  max-width: 1080px;
+  display: grid;
+  padding-top: 100px;
+  margin: 0 auto !important;
+  grid-template-columns: minmax(min-content, 590px);
+  justify-content: center;
+  align-items: center;
+  ${media.md`
+    height: 100vh;
+    min-height: 500px;
+    padding-top: 38px;
+    grid-template-columns: 1fr 1fr;
+  `};
+`
+
+const Title = styled.h1`
+  font-size: 12vw;
+  font-weight: 900;
+  text-transform: uppercase;
+  text-align: center;
+  font-family: 'Gentium Book Basic';
+  color: #363636;
+  margin-bottom: 0;
+  ${media.sm`
+    font-size: 4.4rem;
+  `};
+`
+
+const MainInfoWrapper = styled.div`
+  // justify-content: center;
+  text-align: center;
+  padding: 0 5px;
+  // width: 70%;
+`
+
+const RotationalWrapper = styled.div`
+  padding: 0 20px;
+`
+
+const Subtitle = styled.h2`
+  font-size: 18px;
+  font-weight: 600;
+  margin: 10px 0 20px 0;
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+  color: #1976d2;
+`
+
+const About = ({ data }) => (
+  <div
+    style={{
+      background: '#f3f3f3',
+      backgroundImage: `url(${grain_image})`
+    }}
+  >
+    <Container wrap="wrap" maxWidth="1100px" className="row" justifyContent="center">
+      <MainInfoWrapper>
+        <Title>{data.site.siteMetadata.title}</Title>
+        <Subtitle>{data.site.siteMetadata.subtitle}</Subtitle>
+       
+       
+      </MainInfoWrapper>
+
+      
+
+    </Container>
+  </div>
+)
+
+export default props => (
+  <StaticQuery
+  query={graphql`
+  query{
+
+    aboutImage: imageSharp(fluid: { originalName: { regex: "/about-image.jpg/" } }) {
+      sizes(maxWidth: 960) {
+        ...GatsbyImageSharpSizes_tracedSVG
+      }
+    }
     site {
       siteMetadata {
         title
         siteUrl
       }
     }
-    aboutImage: imageSharp(fluid: { originalName: { regex: "/about-image.jpg/" } }) {
-      sizes(maxWidth: 960) {
-        ...GatsbyImageSharpSizes_tracedSVG
-      }
-    }
   }
-`
+`}
+render={data => <about data={data} {...props} />}
 
-export default About
+/>
+)
