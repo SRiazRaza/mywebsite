43bd6e76691c0829e54ec3450733ddb1293bc324
SRiazRaza <riazraza0@gmail.com>
2020-04-05 15:25:32 +0500

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -381,7 +381,7 @@ class Navbar extends React.Component {
             }}
             to="/"
           >
-           <Name />
+           <Title>{title}</Title>
           </TitleWrapper>
           <div>
             <NavbarToggler onClick={this.handleToggle} id="navbarToggler" className={`${menuIsOpen ? 'open' : ''}`}>
