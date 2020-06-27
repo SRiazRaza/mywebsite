78b8ea140f19a58b1f094dd99356b3d25c0084d2
SRiazRaza <riazraza0@gmail.com>
2020-06-27 17:22:40 +0500

    a

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -100,8 +100,8 @@ export default class SEO extends PureComponent {
         <meta property="og:image" content={image} />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dantehemerson" />
-        <meta name="twitter:creator" content="@dantehemerson" />
+        <meta name="twitter:site" content="@S_Riaz_Raza" />
+        <meta name="twitter:creator" content="@S_Riaz_Raza" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
