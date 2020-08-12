4f6f54b6488e5f241fd779dba59a64001b154647
SRiazRaza <riazraza0@gmail.com>
2020-08-12 12:55:09 +0500

    next Setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/`,
+  pathPrefix: "/mywebsite",
   siteMetadata: {
     title: 'Riaz Raza',
     subtitle: 'Cognitive Software Engineer',
