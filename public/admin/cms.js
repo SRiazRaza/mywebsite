4bf9936077a7f1d4a870405e8f933b4eda124cd5
Riaz Raza <riazraza0@gmail.com>
2021-06-02 01:31:24 +0500

    kk

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7083a13c731191a197d7";
+/******/ 	var hotCurrentHash = "f8a9f9366949a5531a8c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -2469,7 +2469,7 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
 
 // extracted by mini-css-extract-plugin
     if(true) {
-      // 1622577209278
+      // 1622579468244
       var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
